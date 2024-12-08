392f588d04d69b4bdc6005d0f17b1167a7710e04
andrew <julieyeng.js@gmail.com>
2024-12-08 19:52:26 +0700

    super impose

--- client/js/index.js
+++ client/js/index.js
@@ -16,6 +16,29 @@ button1.addEventListener("click", (e)=> {
     sock.emit('btnpress')
 }, false);
 
+function toggleVideo() {
+    var x = document.getElementById("video1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function setupVideo () {
+    // get video
+      var video = document.getElementById('video1')
+      // Play the video, this is optional
+    //   video.play();
+      // Add a listener to this video, so that when the video ends, the video is "hidden".
+      video.addEventListener('ended', function() {
+      // hide video
+        video.style.display = "none";
+      })
+    }
+
+    setupVideo();
+
 sock.on('jumpVideo', () => {
     myvideo[0].currentTime = 66;
     myvideo[0].play();
